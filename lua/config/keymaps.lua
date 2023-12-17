f8eed5298fbb6aa5b13a9c456246434641e88683
hvihvi <hugo.vinet@gmail.com>
2023-12-17 14:37:57 +0100

    add custom keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -5,3 +5,9 @@
 -- move selection up of down
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- Up/Down/Next centered
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
